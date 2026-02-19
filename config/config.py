41b3be36c15e5a309ad4443d0bc84f98dace3927
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-19 21:07:24 +0545

    model save directory

--- config/config.py
+++ config/config.py
@@ -6,3 +6,4 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
 PROCESSED_DATA_DIR = ROOT_DIR / "data" / "processed"
+MODEL_SAVE_DIR = ROOT_DIR / "models"
