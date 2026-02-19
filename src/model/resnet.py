8fcd7f58635d61f18adc0db77ba95eea6fd094b9
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-02-19 21:06:53 +0545

    option added to freeze/unfreeze the pretrained model

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,10 +1,15 @@
-def get_trainable_model():
+def get_trainable_model(config):
     model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-
-    for param in model.parameters():
-        param.requires_grad = False
 
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
+
+    if not config["training"]["fine_tune"]:
+        print("Freezing backbone...")
+        for name, param in model.named_parameters():
+            if 'fc' not in name:
+                param.requires_grad = False
+    else:
+        print("Unfreezing all layers...")
 
     return model
--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -1,8 +1,14 @@
+from torch import optim
 def get_optimizer(config, model):
     optimizer_name = config["training"]["optimizer"]
     optimizer_class = getattr(optim, optimizer_name)
 
+    if config["training"]["fine_tune"]:
+        params_to_train = model.parameters()
+    else:
+        params_to_train = model.fc.parameters()
+
     return optimizer_class(
-        model.fc.parameters(),
+        params_to_train,
         **config["training"]["optimizer_params"]
     )
