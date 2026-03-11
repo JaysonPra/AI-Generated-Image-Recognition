30e48d8f1dfcc147e9f6a12d707b90f0e23a044b
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-03-11 15:03:10 +0545

    model_uri spelling correction

--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @st.cache_resource()
 def load_model():
-    model_uri = f"models:/{MODEL_NAME}@champion"
+    model_uri = f"models:/{MODEL_NAME}@champion"    
     model = mlflow.pytorch.load_model(model_uri)
     model.to(DEVICE)
     model.eval()
